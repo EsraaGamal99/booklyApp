401532c452eb91b2489371ae25b927785ca11ffe
esraa <esraa66m@gmail.com>
2023-10-01 03:24:24 +0300

    add rating to boom details

--- lib/Features/home/presintation/view/widgets/book_rating.dart
+++ lib/Features/home/presintation/view/widgets/book_rating.dart
@@ -4,12 +4,13 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
   const BookRating({
-    super.key,
+    super.key,  this.mainAxisAlignment= MainAxisAlignment.start,
   });
-
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
